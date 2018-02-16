0ae939f4967f42185685a24b3e99865a1975553c
Fatih İver <fatih.iver@boun.edu.tr>
2018-02-16 12:30:48 +0300

    Add files via upload

--- Reading Machine Minds.py
+++ Reading Machine Minds.py
@@ -21,16 +21,7 @@
 # the string "None"). 
 #
 # For example, this non-deterministic machine ...
-edges = { (1, 'a') : [2, 3],
-          (2, 'a') : [2],
-          (3, 'b') : [4, 2],
-          (4, 'c') : [5] }
-accepting = [5] 
-# ... accepts exactly one string: "abc". By contrast, this
-# non-deterministic machine: 
-edges2 = { (1, 'a') : [1],
-           (2, 'a') : [2] }
-accepting2 = [2] 
+
 # ... accepts no strings (if you look closely, you'll see that you cannot
 # actually reach state 2 when starting in state 1). 
 
@@ -52,11 +43,34 @@ accepting2 = [2]
 # over all of the keys in the mapping (i.e., over all of the (state,letter)
 # pairs) -- you'll have to write "edges[edge]" to get the destination list. 
 
-def nfsmaccepts(current, edges, accepting, visited):
-    pass
-        # write your code here 
+def nfsmaccepts(current, edges, accepting, visited): 
+    if current in accepting:
+        return ""
+    else:
+        for key in edges.keys():
+            if key[0] == current:
+                for alternative in edges[key]:
+                    if alternative not in visited:
+                        visited.append(alternative)
+                        returned =  nfsmaccepts(alternative, edges, accepting, visited)
+                        if returned != None:
+                            return key[1] + returned
+        return None
 
-        
+            
+
+    # write your code here 
+
+edges = { (1, 'a') : [2, 3],
+          (2, 'a') : [2],
+          (3, 'b') : [4, 2],
+          (4, 'c') : [5] }
+accepting = [5] 
+# ... accepts exactly one string: "abc". By contrast, this
+# non-deterministic machine: 
+edges2 = { (1, 'a') : [1],
+           (2, 'a') : [2] }
+accepting2 = [2] 
 
 # This problem includes some test cases to help you tell if you are on
 # the right track. You may want to make your own additional tests as well.
@@ -64,5 +78,3 @@ print ("Test 1: " + str(nfsmaccepts(1, edges, accepting, []) == "abc"))
 print ("Test 2: " + str(nfsmaccepts(1, edges, [4], []) == "ab")) 
 print ("Test 3: " + str(nfsmaccepts(1, edges2, accepting2, []) == None)) 
 print ("Test 4: " + str(nfsmaccepts(1, edges2, [1], []) == ""))
-
-
