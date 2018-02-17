22b2d7a9572feda9f3332efc98be6d8161062e3f
Fatih İver <fatih.iver@boun.edu.tr>
2018-02-17 23:08:30 +0300

    Add files via upload

---
+++ Identifier.py
@@ -0,0 +1,22 @@
+# Identifier
+
+# Identifiers are textual string descriptions that refer to program elements,
+# such as variables and functions. Write a indentifier token rule for Javascript identifiers.
+
+# The token rule should match:
+
+#   factorial
+#   underscore_separated
+#   mystery
+#   ABC
+
+# The token rule should not match:
+
+#   _starts_wrong
+#   123
+
+
+def t_IDENTIFIER(token):
+    r'(?:[a-z]|[A-Z])(?:[a-z]|[A-Z]|_)*'
+    return token
+
---
+++ Maximum.py
@@ -0,0 +1,27 @@
+# Bonus Practice: Find Max
+
+# This assignment is not graded and we encourage you to experiment. Learning is
+# fun!
+
+# Given a list l and a function f, return the element of l that maximizes f.
+
+# Assume:
+#    l is not empty
+#    f returns a number
+
+# Example:
+
+l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood','Bible']
+f = len
+
+# Try it on your own!
+
+def maximizer(l , f):
+    mapper = list(map(f, l))
+    max_elem = max(mapper)
+    max_index = mapper.index(max_elem)
+    return l[max_index]
+    
+print(maximizer(l, f))
+
+
