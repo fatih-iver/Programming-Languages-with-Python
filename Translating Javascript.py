2faa6af9714fb3bd637caa4c033e1cc4472adc01
Fatih İver <fatih.iver@boun.edu.tr>
2018-02-25 14:52:28 +0300

    Add files via upload

---
+++ Translating Javascript.py
@@ -0,0 +1,26 @@
+# Valid Statements
+
+# Translate the following JavaScript code to Python:
+
+#        function mymin(a, b){
+#            if (a < b){
+#                return a;
+#            } else {
+#                return b;
+#            };
+#        }
+#        
+#        function square(x){
+#            return x * x;
+#        }
+#        
+#        write(mymin(square(-2), square(3)));
+
+
+def mymin(a, b):
+    return a if a < b else b
+
+def square(x):
+    return x*x
+
+print (mymin(square(-2), square(3)))
